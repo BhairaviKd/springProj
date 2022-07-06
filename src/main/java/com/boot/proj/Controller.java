d38ef6e535bd562275c4ee5c87dac4ebbbf8a254
Vasu <vasukadale@gmail.com>
2022-07-06 12:38:32 +0530

    adding demo endpoint

--- src/main/java/com/boot/proj/Controller.java
+++ src/main/java/com/boot/proj/Controller.java
@@ -1,8 +1,13 @@
 package com.boot.proj;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-
+	
+	@GetMapping("/")
+	public String greet() {
+		return "Welcome User";
+	}
 }
